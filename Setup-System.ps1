2b90a09398b6d7b6c47a51e811723bb1ec1a2d59
petru <petru.cervac@amdaris.com>
2022-07-04 14:45:23 +0300

    Add configuration for git

--- Setup-System.ps1
+++ Setup-System.ps1
@@ -1,4 +1,14 @@
+Write-Host 'Setup Powershell ... '
 . "${PSScriptRoot}\powershell\setup.ps1"
+
+Write-Host 'Setup Windows Terminal ... '
 . "${PSScriptRoot}\WindowsTerminal\setup.ps1"
+
+Write-Host 'Setup Neo vim ... '
 . "${PSSCriptRoot}\nvim\setup.ps1"
+
+Write-Host 'Setup Jetbrains ... '
 . "${PSSCriptRoot}\JetBrains\setup.ps1"
+
+Write-Host 'Setup Git ... '
+. "${PSSCriptRoot}\git\setup.ps1"
