9dee8c6d4cf5b9f31bd6fc00b430cf388e5cc51b
petru <petru.cervac@amdaris.com>
2022-07-07 09:23:12 +0300

    Edit powershell/profile.ps1
    Set up autocompletion for nuke build system

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -248,3 +248,12 @@ switch (Get-Module PSReadLine | Select-Object -ExpandProperty Version) {
 	{ $_ -gt '2.1' } { Set-PSReadLineOption -PredictionSource History; break }
 }
 Set-PSReadLineOption -EditMode Windows
+
+
+# Set up nuke auto completion
+Register-ArgumentCompleter -Native -CommandName nuke -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    nuke :complete "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
