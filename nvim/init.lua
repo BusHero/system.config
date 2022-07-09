a128bf235f414084b47550b694f5d60049071e7e
bus1hero <petru.cervac@gmail.com>
2022-07-09 19:54:51 +0300

    Create a docker image for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,6 @@ vim.opt.cc = '80'
 vim.opt.nu = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.syntax = 'on'
+vim.opt.mouse = 'v'
+vim.opt.clipboard = 'unnamed'
 
-vim.cmd([[
-let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
-if empty(glob(data_dir . '/autoload/plug.vim'))
-  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
-]])
