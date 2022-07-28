2fb00eeffed3077eac0a82e822ce0fa2a6010cfe
petru <petru.cervac@amdaris.com>
2022-07-28 17:15:31 +0300

    feat: Update Set-EnvironmentVariables.Tests.ps1
    Add a test to check that the environment variable is set correctly

--- nvim/Set-EnvironmentVariables.ps1
+++ nvim/Set-EnvironmentVariables.ps1
@@ -1,22 +1,12 @@
-function Set-ConfigHome {
-	param (
-		[string]$XdgConfigHome
-	)
-}
+param(
+	[string]
+	$XdgConfigHome
+)
 
-function Get-ConfigHome {
-	param(
-		[string]$XdgConfigHome
-	)
-}
+[System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', $XdgConfigHome, 'User')
+[System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', $XdgConfigHome, 'Process')
 
-function Config-Neovim {
-	param(
-		[string]$ConfigHome
-	)
-}
 
-# [System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', $env:LOCALAPPDATA, 'User')
 # [System.Environment]::SetEnvironmentVariable('XDG_DATA_HOME', $env:LOCALAPPDATA, 'User')
 # [System.Environment]::SetEnvironmentVariable('XDG_RUNTIME_DIR', '', 'User')
 # [System.Environment]::SetEnvironmentVariable('XDG_STATE_HOME', $env:LOCALAPPDATA, 'User')
--- tests/nvim/Set-EnvironmentVariables.Tests.ps1
+++ tests/nvim/Set-EnvironmentVariables.Tests.ps1
@@ -1,14 +1,16 @@
 BeforeAll {
-	. Get-ScriptPath -Path $PSCommandPath -SourceDirectory ''
+	$path = Get-ScriptPath -Path $PSCommandPath -SourceDirectory ''
 }
 
 Describe 'Check Neovim options were set correctly' {
 	BeforeAll {
-		$XdgConfigHome = 'C:\.config'
-		$ConfigPath = "${XdgConfigHome}\nvim"
-		Set-NeovimEnvironmentVariables -XdgConfigHome $XdgConfigHome 
+		$XdgConfigHome = "C:\$(New-Guid)"
 	}
 	It 'Config path should be the set up one' {
-		nvim --headless +"echo stdpath('config')" +q 2>&1 | Should -Be $ConfigPath
+		Mock-EnvironmentVariable -Variable 'XDG_CONFIG_HOME', 'XDG_DATA_HOME' -Targets User, Process -Fixture {
+			. $path -XdgConfigHome $XdgConfigHome
+			[Environment]::GetEnvironmentVariable('XDG_CONFIG_HOME', 'Process') | Should -Be $XdgConfigHome 
+			[Environment]::GetEnvironmentVariable('XDG_CONFIG_HOME', 'User') | Should -Be $XdgConfigHome 
+		}
 	}
 }
