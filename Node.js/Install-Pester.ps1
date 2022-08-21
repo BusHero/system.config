4172595721d5c868a6d51ee19d71f67f2b183380
bus1hero <petru.cervac@gmail.com>
2022-08-21 22:32:54 +0300

    chore: remove unused files

--- Node.js/Install-Pester.ps1
+++
@@ -1,4 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-Install-PackageProvider -Name NuGet -Force
-Install-Module Pester -Force -SkipPublisherCheck
-Import-Module Pester
