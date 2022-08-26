d47d349e58c8146a5b778c9a3c092a4727744c60
bus1hero <petru.cervac@gmail.com>
2022-08-26 21:54:19 +0300

    feat: add firefox to the install script

--- install.ps1
+++ install.ps1
@@ -1,4 +1,10 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install nodejs -y
 choco install neovim -y
+choco install vscode -y
+choco install powershell-core -y
+choco install git -y
+choco install Firefox -y
--- tests/install/setup.Tests.ps1
+++ tests/install/setup.Tests.ps1
@@ -3,8 +3,19 @@ Describe 'software should be installed' -ForEach @(
 	@{ Tool = 'npm' }
 	@{ Tool = 'node' }
 	@{ Tool = 'nvim' }
+	@{ Tool = 'code' }
+	@{ Tool = 'pwsh' }
+	@{ Tool = 'git' }
 ) {
 	It '<tool> is installed' {
 		Get-Command $Tool -ErrorAction Ignore | Should -Not -Be $Null -Because "${Tool} should be installed"
+	}
+}
+
+Describe 'Test paths' -ForEach @(
+	@{ Path = "$($env:ProgramFiles)\Mozilla Firefox" }
+) {
+	It '<Path> should exist' {
+		$Path | Should -Exist
 	}
 }
