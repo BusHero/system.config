e9f7debd548393758392980d3f5def91cd224694
bus1hero <petru.cervac@gmail.com>
2023-01-10 22:20:29 +0200

    fix: fix windows terminal

--- WindowsTerminal/setup.ps1
+++ WindowsTerminal/setup.ps1
@@ -1,3 +1,7 @@
+New-Item -ItemType Directory `
+	-Path "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\" `
+	-Force
+
 Copy-Item `
 	-Path "${PSScriptRoot}\settings.json" `
 	-Destination "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" `
