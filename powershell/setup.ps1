c49d95560b9907018a610e3366f2472b6776b996
bus1hero <petru.cervac@gmail.com>
2023-01-12 22:15:12 +0200

    feat: update powershell setup.ps1

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,3 +1,5 @@
+winget install -e --id Microsoft.PowerShell -v 7.3.0.0
+
 function Set-Profile {
 	param (
 		[string]$BaseProfile,
@@ -5,19 +7,19 @@ function Set-Profile {
 	)
 	New-Item $SpecificProfile -Force | Out-Null
 	Out-File -InputObject ". ${BaseProfile}" `
-			 -FilePath $SpecificProfile `
-			 -Force | Out-Null		
+		-FilePath $SpecificProfile `
+		-Force | Out-Null
 }
 
 $ProfilePath = "${PSScriptRoot}\profile.ps1"
 
 Set-Profile -BaseProfile $ProfilePath `
-			-SpecificProfile "${env:USERPROFILE}\Documents\PowerShell\profile.ps1"
+	-SpecificProfile "${env:USERPROFILE}\Documents\PowerShell\profile.ps1"
 Set-Profile -BaseProfile $ProfilePath `
-			-SpecificProfile "${env:USERPROFILE}\Documents\WindowsPowerShell\profile.ps1"
+	-SpecificProfile "${env:USERPROFILE}\Documents\WindowsPowerShell\profile.ps1"
 
 # Setup modules
-#  - PSScriptAnalyzer 
+#  - PSScriptAnalyzer
 #  - PSCX
 #  - Pester
 #  - Install-Font
