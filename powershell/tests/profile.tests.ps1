da6904e38bf9387d55a85ac8ce5d9488cc9267b9
bus1hero <petru.cervac@gmail.com>
2023-01-15 12:59:55 +0200

    update tests.ps1

---
+++ powershell/tests/profile.tests.ps1
@@ -0,0 +1,33 @@
+Describe 'powershell' {
+	It 'Powershell core is installed' {
+		Get-Command 'pwsh' -ErrorAction Ignore | Should -Not -BeNullOrEmpty -Because 'PowerShell Core should be installed'
+	}
+
+	Describe "Check profile for '<_>'" -ForEach @(
+		'powershell',
+		'pwsh'
+	) {
+		BeforeAll {
+			$profilePath = & $_ -NoProfile -C '$PROFILE.CurrentUserAllHosts'
+		}
+
+		It 'Profile Path exists' {
+			$profilePath | Should -Exist
+		}
+
+		It 'Profile should have the right data' {
+			$first = Get-Content `
+				-Path $profilePath `
+				-ErrorAction Ignore
+			$second = Get-Content `
+				-Path "${PSScriptRoot}\..\profile.ps1" `
+				-ErrorAction Ignore
+			Compare-Object `
+				-ReferenceObject $first `
+				-DifferenceObject $second `
+				-CaseSensitive `
+				-OutVariable result
+			$result | Should -BeNullOrEmpty -Because 'files should be the same'
+		}
+	}
+}
