b065cfb583823febc5de9777a0efeed6b46a31f2
bus1hero <petru.cervac@gmail.com>
2023-01-15 13:15:01 +0200

    update powershell\setup.ps1

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,11 +1,28 @@
-$profilePath = "${PSScriptRoot}\profile.ps1"
-$powershellProfilePath = powershell -NoProfile -C '$PROFILE.CurrentUserAllHosts'
-$powershellCoreProfilePath = pwsh -NoProfile -C '$PROFILE.CurrentUserAllHosts'
+function Copy-Profile ([string] $path, [string]$destination) {
+	$parent = Split-Path `
+		-Path $destination `
+		-Parent
+	New-Item `
+		-Path $parent `
+		-ItemType Directory `
+		-Force > $null
+	Copy-Item `
+		-Path $path `
+		-Destination $destination
+}
 
-Copy-Item `
+$profilePath = "${PSScriptRoot}\profile.ps1"
+$powershellProfilePath = powershell `
+	-NoProfile `
+	-Command '$PROFILE.CurrentUserAllHosts'
+$powershellCoreProfilePath = pwsh `
+	-NoProfile `
+	-Command '$PROFILE.CurrentUserAllHosts'
+
+Copy-Profile `
 	-Path $profilePath `
 	-Destination $powershellProfilePath
 
-Copy-Item `
+Copy-Profile `
 	-Path $profilePath `
 	-Destination $powershellCoreProfilePath
