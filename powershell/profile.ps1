f498bac185e6f850aa8a40cb99149ba793418362
bus1hero <petru.cervac@gmail.com>
2023-01-16 23:46:25 +0200

    chore: format IsVirtualTerminalProcessingEnabled

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,6 +1,31 @@
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
+function IsVirtualTerminalProcessingEnabled {
+	$MethodDefinitions = @'
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern IntPtr GetStdHandle(int nStdHandle);
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+'@
+	$Kernel32 = Add-Type `
+		-MemberDefinition $MethodDefinitions `
+		-Name 'Kernel32' `
+		-Namespace 'Win32' `
+		-PassThru
+	$hConsoleHandle = $Kernel32::GetStdHandle(-11)
+	$mode = 0
+	$Kernel32::GetConsoleMode($hConsoleHandle, [ref]$mode) >$null
+	if ($mode -band 0x0004) {
+		return $true
+	}
+	return $false
+}
+
+function CanUsePredictionSource {
+	return (! [System.Console]::IsOutputRedirected) -and (IsVirtualTerminalProcessingEnabled)
+}
+
 New-Alias -Name touch -Value New-Item
 
 if ($host.Name -eq 'ConsoleHost') {
@@ -246,30 +271,26 @@ Set-PSReadLineKeyHandler -Key RightArrow `
 		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
 	}
 }
-switch (Get-Module PSReadLine | Select-Object -ExpandProperty Version) {
-	{ $_ -gt '2.2' } {
-		if ([System.Console]::IsOutputRedirected) {
+if (CanUsePredictionSource) {
+	switch (Get-Module PSReadLine | Select-Object -ExpandProperty Version) {
+		{ $_ -gt '2.2' } {
+			Set-PSReadLineOption `
+				-PredictionSource History `
+				-PredictionViewStyle ListView `
+				-WarningAction Ignore `
+				-ErrorAction Ignore
 			break
 		}
-		Set-PSReadLineOption `
-			-PredictionSource History `
-			-PredictionViewStyle ListView `
-			-WarningAction Ignore `
-			-ErrorAction Ignore
-		break
-	}
-	{ $_ -gt '2.1' } {
-		if ([System.Console]::IsOutputRedirected) {
+		{ $_ -gt '2.1' } {
+			Set-PSReadLineOption `
+				-PredictionSource History `
+				-ErrorAction Ignore `
+				-WarningAction Ignore
 			break
 		}
-		Set-PSReadLineOption `
-			-PredictionSource History `
-			-ErrorAction Ignore `
-			-WarningAction Ignore
-		break
 	}
+	Set-PSReadLineOption -EditMode Windows
 }
-Set-PSReadLineOption -EditMode Windows
 
 
 # Set up nuke auto completion
