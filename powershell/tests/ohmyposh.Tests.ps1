29d79ed5b03796b50ee47ce09aaf8dbf820709b1
bus1hero <petru.cervac@gmail.com>
2023-01-16 22:35:58 +0200

    move ohmyposh tests to separate file

---
+++ powershell/tests/ohmyposh.Tests.ps1
@@ -0,0 +1,26 @@
+Describe 'ohmyposh' {
+	It 'Oh my posh is accessible in path' {
+		Get-Command `
+			-Name 'oh-my-posh' `
+			-ErrorAction Ignore | Should -Not -BeNullOrEmpty -Because "'oh-my-posh' should be setup in path"
+	}
+	
+	It 'oh my posh file exists' {
+		"$($Env:USERPROFILE)\.config\ohmyposh\settings.json" | Should -Exist
+	}
+
+	It 'right settings' {
+		$first = Get-Content `
+			-Path "$($env:USERPROFILE)\.config\ohmyposh\settings.json" `
+			-ErrorAction Ignore
+		$second = Get-Content `
+			-Path "${PSScriptRoot}\..\ohmyposh\settings.json" `
+			-ErrorAction Ignore
+		Compare-Object `
+			-ReferenceObject $first `
+			-DifferenceObject $second `
+			-CaseSensitive `
+			-OutVariable result
+		$result | Should -BeNullOrEmpty -Because 'files should be the same'
+	}
+}
