265c51b9857d951c9bdfd8884e78bdbb294168ec
bus1hero <petru.cervac@gmail.com>
2023-01-17 23:36:39 +0200

    move autocompletions to a separate file

---
+++ powershell/ProfileScripts/autocompletion.ps1
@@ -0,0 +1,29 @@
+# Set up nuke auto completion
+Register-ArgumentCompleter -Native -CommandName nuke -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+	nuke :complete "$wordToComplete" | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+	dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
+
+# PowerShell parameter completion shim for winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+	param($wordToComplete, $commandAst, $cursorPosition)
+	[Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+	$Local:word = $wordToComplete.Replace('"', '""')
+	$Local:ast = $commandAst.ToString().Replace('"', '""')
+	winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
+
+# PowerShell parameter completion shim for the GitHub CLI
+Invoke-Expression -Command $(gh completion -s powershell | Out-String)
