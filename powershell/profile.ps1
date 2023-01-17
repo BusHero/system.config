a1075feb86eceadf5bc106a4c84d0511f4e0a150
bus1hero <petru.cervac@gmail.com>
2023-01-17 23:43:03 +0200

    add some more autocompletion

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -38,23 +38,6 @@ oh-my-posh `
 	--shell pwsh `
 	--config "$($env:USERPROFILE)\.config\ohmyposh\settings.json" | Invoke-Expression
 
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-	param($wordToComplete, $commandAst, $cursorPosition)
-	[Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Tzterext.Utf8Encoding]::new()
-	$Local:word = $wordToComplete.Replace('"', '""')
-	$Local:ast = $commandAst.ToString().Replace('"', '""')
-	winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-	}
-}
-
-# PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-	param($commandName, $wordToComplete, $cursorPosition)
-	dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-	}
-}
 
 if (CanUsePredictionSource) {
 	switch (Get-Module PSReadLine | Select-Object -ExpandProperty Version) {
