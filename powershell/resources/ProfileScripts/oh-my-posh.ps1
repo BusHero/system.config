a98bd24dd10f9b428b216759f48a255878515839
bus1hero <petru.cervac@gmail.com>
2023-01-18 23:43:57 +0200

    asd

--- powershell/resources/ProfileScripts/oh-my-posh.ps1
+++ powershell/resources/ProfileScripts/oh-my-posh.ps1
@@ -1,4 +1,8 @@
-oh-my-posh `
-	--init `
-	--shell pwsh `
-	--config "$($env:USERPROFILE)\.config\ohmyposh\settings.json" | Invoke-Expression
+if (Get-Command `
+		-Name 'oh-my-posh' `
+		-ErrorAction Ignore) {
+	oh-my-posh `
+		--init `
+		--shell pwsh `
+		--config "$($env:USERPROFILE)\.config\ohmyposh\settings.json" | Invoke-Expression
+}
--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -37,17 +37,17 @@ powershell `
 	-NoProfile `
 	-Command "$SetExecutionPolicy"
 
-if (! (Get-Command -Name 'oh-my-posh' -ErrorAction Ignore)) {
-	if (Get-Command -Name 'winget' -ErrorAction Ignore) {
-		winget install JanDeDobbeleer.OhMyPosh -s winget
-	}
-	elseif (Get-Command -Name 'scoop' -ErrorAction Ignore) {
-		scoop install 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json'
-	}
-	else {
-		Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-	}
-}
+# if (! (Get-Command -Name 'oh-my-posh' -ErrorAction Ignore)) {
+# 	if (Get-Command -Name 'winget' -ErrorAction Ignore) {
+# 		winget install JanDeDobbeleer.OhMyPosh -s winget
+# 	}
+# 	elseif (Get-Command -Name 'scoop' -ErrorAction Ignore) {
+# 		scoop install 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json'
+# 	}
+# 	else {
+# 		Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+# 	}
+# }
 
 $profilePath = "${PSScriptRoot}\resources\profile.ps1"
 $powershellProfilePath = powershell `
