0634acebff988e7a35df9fc0fee3912c58c078bb
bus1hero <petru.cervac@gmail.com>
2023-01-21 11:17:12 +0200

    chore: add more tests
    add tests to check for validity of windows terminal paths in
    setting.json

--- WindowsTerminal/tests/settings.tests.ps1
+++ WindowsTerminal/tests/settings.tests.ps1
@@ -1,3 +1,21 @@
+BeforeDiscovery {
+	$json = Get-Content -Path "${PSScriptRoot}\..\resources\settings.json" | ConvertFrom-Json
+	$paths = $json.profiles.list | `
+		ForEach-Object { $_.backgroundImage, $_.startingDirectory, $_.icon } | `
+		Where-Object { $_ } | `
+		Where-Object { ! $_.StartsWith('ms-appx:') }
+}
+
+Describe '<_>' -ForEach $paths {
+	BeforeAll {
+		$path = cmd /c echo $_
+	}
+
+	It 'valid' {
+		$path | Should -Exist
+	}
+}
+
 Describe 'WindowsTerminal.settings.json' {
 	BeforeAll {
 		$path = "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
@@ -20,15 +38,11 @@ Describe 'WindowsTerminal.settings.json' {
 		Test-Json -Json $json -Schema $schema | Should -BeTrue
 	}
 
-	It 'Path are valid' {
-		$true | Should -BeFalse # TODO implement the test. The idea is to get all the pats from the settings and check whether they are valid
-	}
+	# It 'Commands are valid' {
+	# 	$true | Should -BeFalse # TODO implement the test. The idea is to get all commands and check that they are valid
+	# }
 
-	It 'Commands are valid' {
-		$true | Should -BeFalse # TODO implement the test. The idea is to get all commands and check that they are valid
-	}
-
-	It 'terminal should be installed' {
-		$true | Should -BeFalse # TODO implement the test
-	}
+	# It 'terminal should be installed' {
+	# 	$true | Should -BeFalse # TODO implement the test
+	# }
 }
