344a92543123728f9adcc2dca917d990d11bc98e
bus1hero <petru.cervac@gmail.com>
2023-01-21 21:47:01 +0200

    feat: add more stuff to chocolately

--- chocolately/setup.ps1
+++ chocolately/setup.ps1
@@ -9,3 +9,17 @@ if (!(Get-Command `
 		-WindowStyle Hidden `
 		-Wait
 }
+
+$foo = pwsh -NoProfile -Command 'Split-Path -Path $PROFILE.CurrentUserAllHosts'
+$bar = powershell -NoProfile -Command 'Split-Path -Path $PROFILE.CurrentUserAllHosts'
+
+foreach ($path in $($foo, $bar)) {
+	New-Item `
+		-Path "${path}\ProfileScripts" `
+		-ItemType Directory `
+		-Force > $null
+
+	Copy-Item `
+		-Path "${PSScriptRoot}\resources\choco.ps1" `
+		-Destination "${path}\ProfileScripts\"
+}
