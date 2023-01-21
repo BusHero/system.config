cc1b84107a3a8bfdb530b3c8cbc79ae3489b7933
bus1hero <petru.cervac@gmail.com>
2023-01-21 17:30:15 +0200

    update install.ps1 for windows terminal

--- WindowsTerminal/scripts/install.ps1
+++ WindowsTerminal/scripts/install.ps1
@@ -9,6 +9,13 @@ if (Get-Command -Name 'winget' -ErrorAction Ignore -WarningAction Ignore) {
 	winget install -e --id Microsoft.WindowsTerminal
 }
 elseif (Get-Command -Name 'choco' -ErrorAction Ignore -WarningAction Ignore) {
+	Add-AppxPackage 'Microsoft.WindowsTerminal_Win10_1.15.3465.0_8wekyb3d8bbwe.msixbundle'
+	Import-Module Appx -UseWindowsPowerShell
+	$temp = New-TemporaryFile
+	Invoke-WebRequest `
+		-Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx `
+		-OutFile $temp.FullName
+	Add-AppxPackage $temp.FullName
 	choco install microsoft-windows-terminal
 }
 elseif (Get-Command -Name 'scoop' -ErrorAction Ignore -WarningAction Ignore) {
