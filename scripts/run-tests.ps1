6c6409cb152a4185fb53e7649846c1cafd8dec35
bus1hero <petru.cervac@gmail.com>
2023-01-22 22:47:00 +0200

    remove scripts folder

--- scripts/run-tests.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module Pester
-
-[PesterConfiguration] $configuration = New-PesterConfiguration
-
-$configuration.Run.Path = "${PSScriptRoot}\..\tests"
-$configuration.Run.Exit = $true
-
-Invoke-Pester -Configuration $configuration
--- scripts/setup.ps1
+++
@@ -1,2 +0,0 @@
-Write-Host 'Setup Powershell ... '
-. "${PSScriptRoot}\..\powershell\setup.ps1"
