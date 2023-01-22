ea7cd0b7f117391af0d52777046fb891863ba11a
bus1hero <petru.cervac@gmail.com>
2023-01-22 23:47:08 +0200

    update test.ps1

--- test.ps1
+++ test.ps1
@@ -8,19 +8,19 @@ Write-Output 'WindowsTerminal ... '
 & "${PSScriptRoot}\WindowsTerminal\test.ps1"
 
 Write-Host 'Setup chocolately ... '
-& "${PSScriptRoot}\chocolately\setup.ps1"
+& "${PSScriptRoot}\chocolately\test.ps1"
 
 Write-Host 'Setup winget ... '
-& "${PSScriptRoot}\winget\setup.ps1"
+& "${PSScriptRoot}\winget\test.ps1"
 
 Write-Host 'Setup dotnet ... '
-& "${PSScriptRoot}\dotnet\setup.ps1"
+& "${PSScriptRoot}\dotnet\test.ps1"
 
 Write-Host 'Setup python ... '
-& "${PSScriptRoot}\python\setup.ps1"
+& "${PSScriptRoot}\python\test.ps1"
 
 Write-Host 'Setup VSCode ... '
-& "${PSScriptRoot}\code\setup.ps1"
+& "${PSScriptRoot}\code\test.ps1"
 
 Write-Host 'Setup sysinternals ... '
-& "${PSScriptRoot}\sysinternals\setup.ps1"
+& "${PSScriptRoot}\sysinternals\test.ps1"
