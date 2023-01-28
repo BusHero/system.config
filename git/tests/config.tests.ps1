3862064975c72f42c60be1b94a4b402b0f49639b
foo <buu>
2023-01-28 20:54:41 +0200

    fix nuke

--- git/tests/config.tests.ps1
+++ git/tests/config.tests.ps1
@@ -14,3 +14,5 @@ Describe 'config' {
 		Set-Location -Path $PSScriptRoot
 	}
 }
+
+
--- nuke/setup.ps1
+++ nuke/setup.ps1
@@ -1,3 +1,17 @@
 if (!(Get-Command -Name nuke -ErrorAction Ignore -WarningAction Ignore)) {
 	dotnet tool install -g nuke.GlobalTool
 }
+
+$foo = pwsh -NoProfile -Command 'Split-Path -Path $PROFILE.CurrentUserAllHosts'
+$bar = powershell -NoProfile -Command 'Split-Path -Path $PROFILE.CurrentUserAllHosts'
+
+foreach ($path in $($foo, $bar)) {
+	New-Item `
+		-Path "${path}\ProfileScripts" `
+		-ItemType Directory `
+		-Force > $null
+
+	Copy-Item `
+		-Path "${PSScriptRoot}\resources\nuke.ps1" `
+		-Destination "${path}\ProfileScripts\"
+}
