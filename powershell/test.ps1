5eb978fbead2191e210d5f68e2452ac6f5d2e06c
foo <buu>
2023-05-13 00:27:38 +0300

    chore: change Version to MinimumVersion

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -1,11 +1,11 @@
 Write-Host 'Powershell core tests ... '
 pwsh -Command '& {
-	Import-Module Pester -Version 5.4.1
+	Import-Module Pester -MinimumVersion 5.0.0
 	Invoke-Pester '${PSScriptRoot}\tests"
 }"
 
 Write-Host 'Powershell tests ... '
 powershell -Command '& {
-	Import-Module Pester -Version 5.4.1
+	Import-Module Pester -MinimumVersion 5.0.0
 	Invoke-Pester '${PSScriptRoot}\tests"
 }"
