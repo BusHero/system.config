167fc9e7312cf7bf53aaf2397fa824ef9e1276de
foo <buu>
2025-05-25 16:35:42 +0300

    chore: configure Windows Terminal

--- WindowsTerminal/setup.ps1
+++ WindowsTerminal/setup.ps1
@@ -1,13 +1,45 @@
-& "${PSScriptRoot}\scripts\install.ps1"
-& "${PSScriptRoot}\scripts\sync.ps1"
+# & "${PSScriptRoot}\scripts\install.ps1"
+# & "${PSScriptRoot}\scripts\sync.ps1"
 
-New-Item `
-	-Path "$($env:USERPROFILE)\.config\" `
-	-ItemType Directory `
-	-Force > $null
+function New-BackupFile([string] $File) {
+	if (-not(Test-Path -Path $File)) {
+		return
+	}
+	$backup = "${File}_$(Get-Date -Format "yyyyMMdd_HHmmss")"
 
-Copy-Item `
-	-Path "${PSScriptRoot}\resources\assets" `
-	-Destination "$($env:USERPROFILE)\.config\WindowsTerminal" `
-	-Recurse `
-	-Force
+	Write-Host 'Moving ' -NoNewline
+	Write-Host $File -ForegroundColor Blue -NoNewline
+	Write-Host ' to ' -NoNewline
+	Write-Host $backup -ForegroundColor Blue -NoNewline
+	Move-Item `
+		-Path $File `
+		-Destination $backup `
+		> $null
+
+	Write-Host ' ✅ Done' -ForegroundColor Green
+}
+
+function New-SymbolicLinkWithBackup(
+	[string] $File,
+	[string] $Target) {
+	New-BackupFile -File $File
+
+	Write-Host "Link " -NoNewline
+	Write-Host $File -ForegroundColor Blue -NoNewline
+	Write-Host ' to ' -NoNewline
+	Write-Host $Target -ForegroundColor Blue -NoNewline
+	New-Item `
+		-Path $File `
+		-ItemType SymbolicLink `
+		-Target $Target `
+		-Force `
+		> $null
+	Write-Host ' ✅ Done' -ForegroundColor Green
+}
+
+New-SymbolicLinkWithBackup `
+	-File "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" `
+	-Target "${PSScriptRoot}\resources\settings.json"
+New-SymbolicLinkWithBackup `
+	-File "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" `
+	-Target "${PSScriptRoot}\resources\settings.json"
